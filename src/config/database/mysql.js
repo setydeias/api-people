181da4e1d5143fff3fc61bf2caf490d9353d23d5
Neto Marçal <neto.marcal.dev@gmail.com>
2022-12-30 17:09:35 -0300

    Implementação do cadastro de Pessoa

--- src/config/database/mysql.js
+++ src/config/database/mysql.js
@@ -1,4 +1,6 @@
-/*var pool = mysql.createPool({
+const mysql = require('mysql');
+
+var pool = mysql.createPool({
     "user"    : process.env.MYSQL_USER,
     "password": process.env.MYSQL_PASSWORD,
     "database": process.env.MYSQL_DATABASE,
@@ -6,11 +8,4 @@
     "port"   : process.env.MYSQL_PORT,
     //"connectionLimit" : 100,
     //"multipleStatements": treu  
-});*/
-
-exports.connection = {
-    "user"    : process.env.MYSQL_USER,
-    "password": process.env.MYSQL_PASSWORD,
-    "database": process.env.MYSQL_DATABASE,
-    "host"    : process.env.MYSQL_HOST
-}
+});
--- src/controllers/people.controller.js
+++ src/controllers/people.controller.js
@@ -1,5 +1,7 @@
 const mysql = require('mysql2/promise');
-const dataconection = require('../config/database/mysql').connection;
+const dataconection = require('../config/database/mysql2').connection;
+const bcrypt = require('bcrypt');
+const { any } = require('bluebird');
 
 exports.postPeople = async (req, res, next) => { 
  
@@ -10,7 +12,41 @@ exports.postPeople = async (req, res, next) => {
   
   try {
 
-    await connection.execute(`
+    await insertPeople(connection, req);    
+    const [rows,] = await connection.execute('SELECT LAST_INSERT_ID() as id_people');
+
+    await insertAdress(connection, req, rows[0].id_people);
+
+    for (let i = 0; i < req.body.contacts.length; i++) {
+      await insertContact(connection, req, rows[0].id_people, i);
+    }
+
+    const temporary_password = parseInt(Math.random() * 999999);
+    
+    await insertUser(connection, req, rows[0].id_people, temporary_password);
+
+    const response = {
+      message: 'Cadastro realizado com sucesso!',
+      pessoaCadastrada: {
+        id_people: rows[0].id_people,
+        name: req.body.name,
+        description_user: req.body.document,
+        password_user: temporary_password,
+      }
+    }
+
+    await connection.commit();
+    return res.status(201).send(response);
+
+  } catch (err) {
+    connection.rollback();
+    console.error(`Não foi possível cadastrar: ${err.message}`, err);
+    return res.status(304).send(err);
+  }
+}
+
+const insertPeople = (connection, req) => {
+  connection.execute(`
       INSERT INTO people(
         id_status, id_document_type, document, usual_name, birth_date, sexo, id_treatment, date_registration
       )
@@ -27,11 +63,38 @@ exports.postPeople = async (req, res, next) => {
         req.body.id_treatment, 
         req.body.date_registration
     ]);   
-    
-    const [rows,] = await connection.execute('SELECT LAST_INSERT_ID() as id_people');
-    
-    for (let i = 0; i < req.body.contacts.length; i++) {
-      await connection.execute(`
+}
+
+const insertAdress = (connection, req, id_people) => {
+  connection.execute(`
+      INSERT INTO adress(
+        id_people, 
+        id_adress_type, 
+        adress, 
+        address_number, 
+        city, 
+        uf, 
+        address_complement, 
+        district, 
+        cep
+      )
+      VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?)
+
+    `, [
+      id_people,
+      req.body.id_adress_type,
+      req.body.adress,
+      req.body.address_number,
+      req.body.city,
+      req.body.uf,
+      req.body.address_complement,
+      req.body.district,
+      req.body.cep
+    ]);    
+}
+
+const insertContact = (connection, req, id_people, i) => {
+  connection.execute(`
         INSERT INTO contact(
           id_people,
           id_contact_type,
@@ -43,15 +106,21 @@ exports.postPeople = async (req, res, next) => {
           ?,?,?,?,?
         );
       `,[      
-        rows[0].id_people,
+        id_people,
         req.body.contacts[i].id_contact_type,
         req.body.contacts[i].contact,
         req.body.contacts[i].whatsapp,
         req.body.contacts[i].main
       ]);       
-    }
+}
 
-    await connection.execute(`
+const insertUser = (connection, req, id_people, temporary_password) => {
+  bcrypt.hash( toString(temporary_password), 10, (errBcrypt, hash) => {
+    if(errBcrypt) { 
+      connection.rollback();
+      return res.status(500).send({ error: errBcrypt })
+    }
+    connection.execute(`
       INSERT INTO user(
         id_people,
         description_user,
@@ -61,27 +130,9 @@ exports.postPeople = async (req, res, next) => {
         ?, ?, ?
       );
     `,[      
-      rows[0].id_people,
-      req.body.description_user,
-      req.body.password_user
+      id_people,
+      req.body.document,
+      hash
     ]);
-
-
-    const response = {
-      message: 'Cadastro realizado com sucesso!',
-      pessoaCadastrada: {
-        d_people: rows[0].id_people,
-        name: req.body.name
-      }
-    }
-
-    await connection.commit();
-    return res.status(201).send(response);
-
-  } catch (err) {
-    connection.rollback();
-    console.error(`Ocorreu um erro ao criar o pedido: ${err.message}`, err);
-    return res.status(304).send(err.message);
-  }
+  });
 }
-
